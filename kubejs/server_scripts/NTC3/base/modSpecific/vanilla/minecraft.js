37fc8baf095bce5239cbc647cb15f0cea53fb4af
MechOrigin <matt__tatum@msn.com>
2022-06-14 20:56:54 -0700

    test

--- kubejs/server_scripts/NTC3/base/modSpecific/vanilla/minecraft.js
+++ kubejs/server_scripts/NTC3/base/modSpecific/vanilla/minecraft.js
@@ -1,13 +1,13 @@
 onEvent('recipes', (event) => {
 
-    removeRecipeByID(e, [
+    removeRecipeByID(event, [
         'nethers_exoticism:crafting_table_recipe_1',
         'nethers_exoticism:crafting_table_recipe_2'
       ])
 
     modifyShaped(event, 'minecraft:crafting_table', 1, ['AA ', 'BB ', '   '], {
-        A: 'minecraft:planks',
-        B: 'minecraft:flint'
+        A: '#minecraft:planks',
+        B: '#forge:gems/flint'
       })
 
     //modifyShapeless(event, 'minecraft:crafting_table', 1, ['minecraft:planks', '#appliedenergistics2:knife'])
